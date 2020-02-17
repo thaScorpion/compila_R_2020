9c6d0b0c55bcb04e44670763a5b9923a7f5c62aa
Simo-Gh <52351661+Simo-Gh@users.noreply.github.com>
2020-02-17 22:03:12 +0100

    Update syn.h

--- realisation/syn.h
+++ realisation/syn.h
@@ -1,1 +1,35 @@
-syn.h
+#ifndef ANALYSEUR_SYNTHAXIQUE_H
+#define ANALYSEUR_SYNTHAXIQUE_H
+
+
+#include "analyseur_lexical.h"
+
+
+//declaration 
+
+
+
+
+
+void Test_Symbole(CODE_LEX code,CODE_ERR error);
+
+void INSTS();
+void INST();
+void AFFEC1();
+void SI();
+void COND();
+void TANTQUE();
+void REPEAT();
+void POUR();
+void ECRIRE();
+
+void EXPR();
+void TERM();
+void FACT();
+
+
+
+
+
+
+#endif
